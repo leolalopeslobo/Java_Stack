7fdd604d8208950c4e63f25c0231f1c932f081cb
leolalopeslobo <leolalopeslobo@gmail.com>
2023-08-31 10:33:20 +0530

    commit 4

--- react-hooks-frontend/react-hooks-frontend/src/App.js
+++ react-hooks-frontend/react-hooks-frontend/src/App.js
@@ -1,12 +1,15 @@
 //import logo from './logo.svg';
 import './App.css';
+import FooterComponent from './components/FooterComponent';
+import HeaderComponent from './components/HeaderComponent';
 import ListEmpoloyeeComponent from './components/ListEmpoloyeeComponent';
 
 function App() {
   return (
     <div>
+      <HeaderComponent />
       <ListEmpoloyeeComponent />
-      
+      <FooterComponent />
     </div>
   );
 }
